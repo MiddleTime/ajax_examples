0d36a52430ad0ac76a71d0e24d7920458a903b39
Vasya <trata@yahoo.com>
2019-11-03 22:10:52 +0200

    personal use

---
+++ ex3/script/script.js
@@ -0,0 +1,14 @@
+
+document.querySelector('button').onclick = function () {
+    let input = document.querySelector('#inp-date');
+    let date = { "date": input.value };
+    ajax('datetime.php', 'POST', showTime, date);
+}
+
+
+
+function showTime(data) {
+    data = JSON.parse(data);
+    console.log(data);
+    alert(data.F);
+}
